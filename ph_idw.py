8dc7918b6a0c59241edb4ad3e5def7b07bc033ae
Cahlil-Togonon <100193550+Cahlil-Togonon@users.noreply.github.com>
2023-02-20 18:02:32 +0800

    change filepath of shapefiles

--- ph_idw.py
+++ ph_idw.py
@@ -1,8 +1,8 @@
 from pyidw import idw
 
 idw.idw_interpolation(
-    input_point_shapefile="Philippines_Pollution.shp",
-    extent_shapefile="Philippines_Border.shp",
+    input_point_shapefile="./shapefiles/Philippines_Pollution.shp",
+    extent_shapefile="./shapefiles/Philippines_Border.shp",
     column_name="US AQI",
     power=2,
     search_radious=15,

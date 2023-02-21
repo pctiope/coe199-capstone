480329fd02112765414ba35f1ed7ecce1e9487c5
Cahlil-Togonon <100193550+Cahlil-Togonon@users.noreply.github.com>
2023-02-21 12:57:11 +0800

    started adding iqair sensor locations

--- ph_iqair.py
+++ ph_iqair.py
@@ -1,26 +1,45 @@
 import requests
 import pandas as pd
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 from time import time
 
 IQAir_sensors = {
-    "Multinational_Paranaque" : "https://www.iqair.com/philippines/ncr/paranaque/multinational-village",#waqi
-    "Calzada_Taguig" : "https://www.iqair.com/philippines/ncr/taguig/calzada",#waqi
-    "Forbestown_Taguig" : "https://www.iqair.com/philippines/ncr/taguig/forbestown-road",#waqi
+    # "Multinational_Paranaque" : "https://www.iqair.com/philippines/ncr/paranaque/multinational-village", #waqi
+    # "Calzada_Taguig" : "https://www.iqair.com/philippines/ncr/taguig/calzada", #waqi
+    # "Forbestown_Taguig" : "https://www.iqair.com/philippines/ncr/taguig/forbestown-road", #waqi
     "NorthForbesPark_Makati" : "https://www.iqair.com/philippines/ncr/makati/north-forbes-park",
     "Dasmarinas_Makati" : "https://www.iqair.com/philippines/ncr/makati/dasmarinas-village",
     "AyalaCircuit_Makati" : "https://www.iqair.com/philippines/ncr/makati/circuit-ayala-outside",
-    "WackWack_Mandaluyong" : "https://www.iqair.com/philippines/ncr/mandaluyong/wack-wack-greenhills",#waqi
+    # "WackWack_Mandaluyong" : "https://www.iqair.com/philippines/ncr/mandaluyong/wack-wack-greenhills", #waqi
     "Unioil_Shaw" : "https://www.iqair.com/philippines/ncr/mandaluyong/unioil-shaw",
     "Unioil_Blumentritt" : "https://www.iqair.com/philippines/ncr/san-juan/unioil-f-blumentritt",
     "Unioil_Cainta" : "https://www.iqair.com/philippines/calabarzon/rizal/unioil-cainta-ortigas-ext",
     "Unioil_Congressional" : "https://www.iqair.com/philippines/ncr/quezon-city/unioil-congressional-2",
     "Unioil_WestAve" : "https://www.iqair.com/philippines/ncr/quezon-city/unioil-west-ave",
-    "Unioil_EdsaGuadalupe" : "https://www.iqair.com/philippines/ncr/makati/unioil-edsa-guadalupe-2",
-    "Unioil_Meycauayan" : "https://www.iqair.com/philippines/central-luzon/meycauayan/unioil-meycauayan-bulacan"    # 0 always?
+    "Unioil_EdsaGuadalupe" : "https://www.iqair.com/philippines/ncr/makati/unioil-edsa-guadalupe-2"
+    # "Unioil_Meycauayan" : "https://www.iqair.com/philippines/central-luzon/meycauayan/unioil-meycauayan-bulacan"    # 0 always?
+    # MISSING: Unioil Katipunan - Quezon Ave - Barangka - Conception, National Children's Hospital, Madison st. Greenhills
+}
+
+IQAir_locations = {
+    # "Multinational_Paranaque" : [121.001488,14.486208], #waqi
+    # "Calzada_Taguig" : [121.07563,14.536089], #waqi
+    # "Forbestown_Taguig" : [121.043945,14.550762], #waqi
+    "NorthForbesPark_Makati" : [121.035596,14.553975],
+    "Dasmarinas_Makati" : [121.030732,14.56628],
+    "AyalaCircuit_Makati" : [0,0],                          # TO GET
+    # "WackWack_Mandaluyong" : [121.05573,14.591224], #waqi
+    "Unioil_Shaw" : [0,0],                                  # TO GET
+    "Unioil_Blumentritt" : [121.026652,14.6021],
+    "Unioil_Cainta" : [121.125296,14.582285],
+    "Unioil_Congressional" : [0,0],                         # TO GET
+    "Unioil_WestAve" : [121.027586,14.644702],
+    "Unioil_EdsaGuadalupe" : [121.025319,14.476359]
+    # "Unioil_Meycauayan" : []    # 0 always?
     # MISSING: Unioil Katipunan - Quezon Ave - Barangka - Conception, National Children's Hospital, Madison st. Greenhills
 }
 

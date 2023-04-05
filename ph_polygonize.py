ae51c6b420eb6ab50d28e1b9409a104d89e2d8fc
Cahlil-Togonon <100193550+Cahlil-Togonon@users.noreply.github.com>
2023-04-05 17:21:05 +0800

    removed out of bounds polygons

--- ph_polygonize.py
+++ ph_polygonize.py
@@ -8,8 +8,7 @@ def polygonize(threshold, date_time):
         with rasterio.open("./shapefiles/Philippines_Pollution_"+date_time+"_idw.tif") as src:
             image = src.read(1) # first band
             image = image.astype('int16')
-            results = ({'type':'Feature','properties': {'AQI': v}, 'geometry': s} for s, v in shapes(image, mask=mask, transform=src.transform))
-    geoms = list(results)
+            geoms = [{'type':'Feature','properties': {'AQI': v}, 'geometry': s} for s,v in shapes(image, mask=mask, transform=src.transform) if v <= 1000]
     output_dict = {"type": "FeatureCollection", "name": "polygonized", "threshold": threshold, "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}}, "features": geoms}
     json_output = json.dumps(output_dict, indent=4)
     with open("./temp/polygonized_"+date_time+".json", "w") as outfile:
